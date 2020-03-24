Add normalization and fix probabilities

lab8/particle_filter.py
@@ -14,7 +14,8 @@ class Command(enum.Enum):
 
 
 class Particle:
-    def __init__(self, pos_x, pos_y, theta, map, weight, distance, sigma_sensor, sigma_direction, sigma_distance, id, initial_prob):
+    def __init__(self, pos_x, pos_y, theta, map, weight, distance, sigma_sensor, sigma_direction, sigma_distance, id,
+                 initial_prob):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.theta = theta
@@ -34,13 +35,14 @@ class Particle:
         # return "Particle id:% d, x:% f, y:% f, θ:% f, weight:% f, prev_prob:% f" \
         #        % (self.id, self.pos_x, self.pos_y, self.theta, self.weight, self.prev_posterior_log_prob)
         return "Particle id:% d, x:% f, y:% f, θ:% f, weight:% f, current_prob:% f" \
-                   % (self.id, self.pos_x, self.pos_y, self.theta, self.weight, self.calculate_partial_posterior_prob())
+               % (self.id, self.pos_x, self.pos_y, self.theta, self.weight, self.calculate_partial_posterior_prob())
 
     def update_sensor_reading(self, sensor_measurement_z):
         self.sensor_reading = sensor_measurement_z
 
     def calculate_partial_posterior_prob(self):
-        print("[particle id: {}, x: {}, y: {}, theta: {}]".format(self.id, self.pos_x, self.pos_y, math.degrees(self.theta)))
+        print("[particle id: {}, x: {}, y: {}, theta: {}]".format(self.id, self.pos_x, self.pos_y,
+                                                                  math.degrees(self.theta)))
         location = self.map.closest_distance((self.pos_x, self.pos_y), self.theta)
         if location is None or location == 0:
             return self.prev_posterior_log_prob
@@ -52,22 +54,23 @@ class Particle:
         return posterior_probability
 
     def move(self, desired_theta=None):
-        # model (maybe variance σ(square) goes here instead of standard deviation)
+        # model
         print("theta: " + str(desired_theta))
         if desired_theta is None:
             desired_theta = self.theta
         normal_random_variable_sample_direction = np.random.normal(0, self.sigma_direction, 1)[0]
         print("[particle id: {}, nrvsdirection: {}]".format(self.id, normal_random_variable_sample_direction))
         theta_prime = desired_theta + normal_random_variable_sample_direction
-        # theta_prime %= 2 * np.pi
 
         normal_random_variable_sample_distance = np.random.normal(0, self.sigma_distance, 1)[0]
         print("[particle id: {}, nrvsdistance: {}]".format(self.id, normal_random_variable_sample_distance))
         distance_prime = self.distance + normal_random_variable_sample_distance
 
         # propagate
-        self.pos_x = clamp(self.pos_x + distance_prime * np.cos(theta_prime), self.map.bottom_left[0], self.map.top_right[0])
-        self.pos_y = clamp(self.pos_y + distance_prime * np.sin(theta_prime), self.map.bottom_left[1], self.map.top_right[1])
+        self.pos_x = clamp(self.pos_x + distance_prime * np.cos(theta_prime), self.map.bottom_left[0],
+                           self.map.top_right[0])
+        self.pos_y = clamp(self.pos_y + distance_prime * np.sin(theta_prime), self.map.bottom_left[1],
+                           self.map.top_right[1])
         self.theta = theta_prime
 
 
@@ -85,7 +88,7 @@ class ParticleFilter:
         self.sigma_distance = sigma_distance
 
         self.particles = []
-        self.weights = [1/num_particles] * num_particles
+        self.weights = [1 / num_particles] * num_particles
 
         for index in range(0, num_particles):
             pos_x = np.random.uniform(0, 3)
@@ -118,7 +121,6 @@ class ParticleFilter:
         self.estimation()
         self.draw_particles()
 
-
     def sensing(self, sensor_measurement_z: float):
         for particle_index in range(0, len(self.particles)):
             particle = self.particles[particle_index]
@@ -126,16 +128,19 @@ class ParticleFilter:
             self.weights[particle_index] = particle.calculate_partial_posterior_prob()
             # print(particle)
 
-        # print("weights before adding logsumexp: ", self.weights)
+        print("weights before adding logsumexp")
+        self.print_particles()
 
         # Compute N and add to have total posterior probability
         self.weights += logsumexp(self.weights)
+
+        print("weights after adding logsumexp")
+
+        self.print_particles()
+
         # self.weights -= logsumexp(self.weights)
         # n_effective = logsumexp(self.weights + self.weights)
         # print("Neff = %.4f" % np.exp(n_effective))
-
-        print("weights before adding logsumexp")
-        self.print_particles()
 
         np.vectorize(set_prev_prob)(self.particles, self.weights)
 
@@ -148,7 +153,6 @@ class ParticleFilter:
         self.estimation()
         self.draw_particles()
 
-    # Analyze
     def estimation(self):
         x_avg = np.average(np.vectorize(lambda particle: particle.pos_x)(self.particles))
 
@@ -158,16 +162,23 @@ class ParticleFilter:
         # draw the estimated position and all other particles
         self.virtual_create.set_pose((x_avg, y_avg, 0.1), theta_avg)
 
-
-    # Analyze
     def resample_particles(self):
-        index = np.random.choice(np.arange(0, len(self.particles)), len(self.particles), replace=True,
-                                 p=np.exp(self.weights))
-        particles = []
-        for i in index:
-            particles.append(copy.deepcopy(self.particles[i]))
+        normalized_weights = self.normalize_weights()
 
-        return particles
+        print("weights after normalizing")
+        print(normalized_weights)
+
+        index = np.random.choice(np.arange(0, len(self.particles)), len(self.particles), replace=False,
+                                 p=normalized_weights)
+
+        print("index after resampling")
+        print(index)
+
+        sampled_particles = []
+        for i in index:
+            sampled_particles.append(copy.deepcopy(self.particles[i]))
+
+        self.particles = sampled_particles
 
     def draw_particles(self):
         data = []
@@ -180,9 +191,18 @@ class ParticleFilter:
         for particle in self.particles:
             print(particle)
 
+    def normalize_weights(self):
+        normalized_weights = [0] * len(self.weights)
+        weight_total_sum = 0
+        for weight in self.weights:
+            weight_total_sum += weight
+        for i in range(0, len(self.weights)):
+            normalized_weights[i] = (self.weights[i]) / weight_total_sum
+        return normalized_weights
+
+
 def clamp(value, range_min, range_max):
-    return value
-    # return max(min(value, range_max), range_min)
+    return max(min(value, range_max), range_min)
 
 
 def set_prev_prob(particle, weight):